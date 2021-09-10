e873b4c65a2fb9364938bb4d643134c4d5f1289a
katoretb <katoretv@gmail.com>
2021-09-10 09:03:17 +0700

    1.1.1

---
+++ shot.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+name = '---' #replace with your name
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("press space to take a photo", 500, 300)
+
+img_counter = 0
+
+while True:
+    ret, img = cam.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("press space to take a photo", img)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, img)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
